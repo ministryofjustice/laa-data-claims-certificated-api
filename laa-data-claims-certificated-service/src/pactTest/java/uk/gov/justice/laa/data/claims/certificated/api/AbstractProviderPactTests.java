bbd01e57faf10c0b4b634586f167340b04972795
andrewJohnysJustice <andrew.johnys@justice.gov.uk>
2026-06-08 14:26:33 +0100

    [DSTEW-1503] - Pact Test structure for certficated claims (#11)
    * DSTEW-1503 - Pact Test structure for certficated claims
    
    * DSTEW-1503 - Pact Test structure for certficated claims - added plumbing
    
    * DSTEW-1503 - cleanup
    
    * DSTEW-1503 - README file updated
    
    * DSTEW-1503 - SpotlessJava
    
    * DSTEW-1503 - SpotlessJava
    
    * DSTEW-1503 - Removed offending build.gradle dependency
    
    * DSTEW-1503 - Added webhook for GitHub Pact test CI-CD
    
    * DSTEW-1503 - Added webhook for recording pact on main branch
    
    * DSTEW-1503 - Updated comment on webhook
    
    * DSTEW-1503 - added pact test action for pipeline
    
    * DSTEW-1503 - fixed typos

---
+++ laa-data-claims-certificated-service/src/pactTest/java/uk/gov/justice/laa/data/claims/certificated/api/AbstractProviderPactTests.java
@@ -0,0 +1,36 @@
+package uk.gov.justice.laa.data.claims.certificated.api;
+
+import jakarta.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.actuate.endpoint.web.PathMappedEndpoints;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import uk.gov.justice.laa.data.claims.certificated.api.repository.ItemRepository;
+
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@TestPropertySource(
+    properties = {
+      "laa.springboot.starter.auth.authentication-header=Authorization",
+    })
+public class AbstractProviderPactTests {
+
+  @BeforeAll
+  static void setupPactProperties() {
+    System.setProperty("pactbroker.host", "localhost");
+    System.setProperty("pactbroker.port", "9292");
+    System.setProperty("pactbroker.scheme", "http");
+    System.setProperty("laa.springboot.starter.exports.enabled", "true");
+  }
+
+  @MockitoBean protected ItemRepository itemRepository;
+
+  // Mocked various DB beans to allow application to run properly without dependencies
+  @MockitoBean protected Flyway flyway;
+  @MockitoBean protected EntityManagerFactory entityManagerFactory;
+  @MockitoBean protected PathMappedEndpoints pathMappedEndpoints;
+  @MockitoBean protected DataSource dataSource;
+}
